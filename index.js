b4092aaff26e893912d73bb9f6a333f7b27a3893
mifwek <44892124+mifwek@users.noreply.github.com>
2018-12-03 07:56:17 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame(`Free Free!`);
+    console.log(`${bot.user.username} sudah siap!`);
+});
+
+bot.on("message", async (message) => {
+	if (message.content.startsWith('halo')) {
+		message.channel.send("halo juga");
+	}
+});
+
+bot.login("process.env.token");
