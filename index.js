2fcf199620217e8290e2d8e78a26b7962674f9bc
mifwek <44892124+mifwek@users.noreply.github.com>
2018-12-05 16:34:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const config = require("./config.json");
 
 bot.on("ready", function() {
     bot.user.setGame(`Free Free!`);
@@ -8,8 +9,26 @@ bot.on("ready", function() {
 
 bot.on('guildMemberAdd', async (member) => {
 	const joinchannel = member.guild.channels.find('name', 'welcomer-goodbye');
-    joinchannel.send(`Welcome to the jungle 👋😹 ${member.user}`);
+    joinchannel.send(`Welcome to the jungle 👋😹 ${member.user.tag}`);
 
+});
+
+bot.on("message", async message => {
+	if (message.author.bot) return;
+	if (message.channel.type === 'dm') return
+
+	let prefix = config.prefix;
+	let messageArray = message.content.split(" ");
+	let cmd = messageArray[0];
+	let args = messageArray.slice(1);
+
+if (cmd === `${prefix}say`) {
+		 if(!message.member.hasPermission("ADMINISTRATOR")) return;
+    let say = args.join(" ");
+    if(!say) return message.reply("masukan sebuah kata atau kalimat");
+          message.delete().catch(O_o=>{}); 
+          message.channel.send(say);
+    }
 });
 
 bot.login(process.env.TOKEN);
